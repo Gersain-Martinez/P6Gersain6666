6adae082312e77e6479f6f71ffc495226b5850db
Gersian-Martinez <martinezgersain30@gmail.com>
2019-10-17 15:09:48 -0500

    Initial commit

---
+++ app/src/main/java/com/gmh/itics/tesoem/edu/p6gersain6666/MainActivity.java
@@ -0,0 +1,20 @@
+package com.gmh.itics.tesoem.edu.p6gersain6666;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void mensaje(View view){
+        Toast.makeText(this, "Mensaje Emergente", Toast.LENGTH_SHORT).show();
+
+    }
+}
